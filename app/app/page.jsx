ab271a8593fa36f2fa5c7a293aa098fff7ccba15
carlosandresagabriel25-dev <carlosandresagabriel25@gmail.com>
2026-09-25 20:24:38 -0300

    Create page.jsx

---
+++ app/app/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>CEF.AI</h1>
+      <p>A plataforma está online e pronta para crescer!</p>
+    </div>
+  )
+}
